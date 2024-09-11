0bacfd14df8f1f29edbbc1bae21191a4ed94e536
Bho0 <JackColombo2004@gmail.com>
2024-09-11 14:26:20 +0200

    updates

--- ship.py
+++ ship.py
@@ -1,4 +1,6 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.popup import Popup
+from kivy.uix.togglebutton import ToggleButton
 from kivy.properties import StringProperty
 
 import json
@@ -24,6 +26,11 @@ class Ship(Screen):
             self.selected_projectile = ''
         else:
             self.selected_projectile = projectile_type
+    
+    def goto_captain(self, timestamp):
+        game_screen = self.manager.get_screen('captain')  # Ottieni il nuovo schermo
+        game_screen.load_screen(timestamp)  # Passa i dati al nuovo schermo
+        self.manager.current = 'captain'  # Cambia schermo
 
 class Captain(Screen):
     selected_dress = StringProperty('')
@@ -34,3 +41,23 @@ class Captain(Screen):
             self.selected_dress = ''
         else:
             self.selected_dress = dress_type
+    
+    def load_screen(self, timestamp):
+        grid = self.ids.cap_grid
+        grid.clear_widgets()  # Pulisci la lista prima di aggiungere nuovi elementi
+
+        filename = 'save_data.json'
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                all_data = json.load(f)
+
+            if timestamp in all_data:
+                save_data = all_data[timestamp]
+
+            for dress in save_data['dresses']:
+                button = ToggleButton(text=f"{dress} dress",
+                                group='C',
+                                background_normal='images\captain.png',
+                                on_press=lambda btn, dr=dress: self.select_dress(dr)
+                                )
+                grid.add_widget(button)
