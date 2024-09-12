bbfc64dd5f6b799999fd99cdff4e43738009c479
Bho0 <JackColombo2004@gmail.com>
2024-09-12 11:46:15 +0200

    updates

--- ship.py
+++ ship.py
@@ -25,12 +25,65 @@ class Ship(Screen):
 class Captain(Screen):
     selected_dress = StringProperty('')
 
-    def select_dress(self, dress_type):
-        
+    def select_dress(self, dress_type, timestamp):
+        filename = 'save_data.json'
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                all_data = json.load(f)
+            
+            if timestamp in all_data:
+                save_data = all_data[timestamp]
+
         if self.selected_dress == dress_type:
             self.selected_dress = ''
+            updated_data = {
+                'name': save_data['name'],
+                'tutorial': save_data['tutorial'],
+                'levels' : save_data['levels'],
+                'coins' : save_data['coins'],
+                'bullet': save_data['bullet'],
+                'bomb': save_data['bomb'],
+                'laser': save_data['laser'],
+                'red_dress': save_data['red_dress'],
+                'blu_dress': save_data['blu_dress'],
+                'green_dress': save_data['green_dress'],
+                'yellow_dress': save_data['yellow_dress'],
+                'selected_dress': 'red',
+                'selected_projectiles': save_data['selected_projectiles']
+            }
+
+            # Aggiungi i nuovi dati al dizionario con timestamp come chiave
+            all_data[timestamp] = updated_data
+            
+            # Salva il dizionario aggiornato nel file JSON
+            with open(filename, 'w') as f:
+                json.dump(all_data, f, indent=4)
+
         else:
             self.selected_dress = dress_type
+            updated_data = {
+                'name': save_data['name'],
+                'tutorial': save_data['tutorial'],
+                'levels' : save_data['levels'],
+                'coins' : save_data['coins'],
+                'bullet': save_data['bullet'],
+                'bomb': save_data['bomb'],
+                'laser': save_data['laser'],
+                'red_dress': save_data['red_dress'],
+                'blu_dress': save_data['blu_dress'],
+                'green_dress': save_data['green_dress'],
+                'yellow_dress': save_data['yellow_dress'],
+                'selected_dress': dress_type,
+                'selected_projectiles': save_data['selected_projectiles']
+            }
+
+            # Aggiungi i nuovi dati al dizionario con timestamp come chiave
+            all_data[timestamp] = updated_data
+            
+            # Salva il dizionario aggiornato nel file JSON
+            with open(filename, 'w') as f:
+                json.dump(all_data, f, indent=4)
+
     
     def load_screen(self, timestamp):
         grid = self.ids.cap_grid
@@ -48,7 +101,7 @@ class Captain(Screen):
                 button = ToggleButton(text=f"red dress",
                                 group='C',
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='red_dress': self.select_dress(dr)
+                                on_press=lambda btn, dr='red', ts=timestamp: self.select_dress(dr,ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -61,7 +114,7 @@ class Captain(Screen):
                 button = ToggleButton(text=f"blu dress",
                                 group='C',
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='blu_dress': self.select_dress(dr)
+                                on_press=lambda btn, dr='blu', ts=timestamp: self.select_dress(dr,ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -74,7 +127,7 @@ class Captain(Screen):
                 button = ToggleButton(text=f"green dress",
                                 group='C',
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='green_dress': self.select_dress(dr)
+                                on_press=lambda btn, dr='green', ts=timestamp: self.select_dress(dr,ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -87,7 +140,7 @@ class Captain(Screen):
                 button = ToggleButton(text=f"yellow dress",
                                 group='C',
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='yellow_dress': self.select_dress(dr)
+                                on_press=lambda btn, dr='yellow', ts=timestamp: self.select_dress(dr,ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -99,11 +152,74 @@ class Captain(Screen):
 class Projectile(Screen):
     selected_projectile = StringProperty('')
 
-    def select_projectile(self, projectile_type):        
+    def select_projectile(self, projectile_type, timestamp):
+        flag = True
+        filename = 'save_data.json'
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                all_data = json.load(f)
+            
+            if timestamp in all_data:
+                save_data = all_data[timestamp]
+
         if self.selected_projectile == projectile_type:
             self.selected_projectile = ''
+            save_data['selected_projectiles'].remove(projectile_type)
+            if save_data['selected_projectiles'] == []:
+                save_data['selected_projectiles'].append('bullet')
+
+            updated_data = {
+                'name': save_data['name'],
+                'tutorial': save_data['tutorial'],
+                'levels' : save_data['levels'],
+                'coins' : save_data['coins'],
+                'bullet': save_data['bullet'],
+                'bomb': save_data['bomb'],
+                'laser': save_data['laser'],
+                'red_dress': save_data['red_dress'],
+                'blu_dress': save_data['blu_dress'],
+                'green_dress': save_data['green_dress'],
+                'yellow_dress': save_data['yellow_dress'],
+                'selected_dress': save_data['selected_dress'],
+                'selected_projectiles': save_data['selected_projectiles']
+            }
+
+            # Aggiungi i nuovi dati al dizionario con timestamp come chiave
+            all_data[timestamp] = updated_data
+            
+            # Salva il dizionario aggiornato nel file JSON
+            with open(filename, 'w') as f:
+                json.dump(all_data, f, indent=4)
+
         else:
             self.selected_projectile = projectile_type
+            for type in save_data['selected_projectiles']:
+                if type == projectile_type:
+                    flag = False
+            if flag == True:
+                save_data['selected_projectiles'].append(projectile_type)
+            updated_data = {
+                'name': save_data['name'],
+                'tutorial': save_data['tutorial'],
+                'levels' : save_data['levels'],
+                'coins' : save_data['coins'],
+                'bullet': save_data['bullet'],
+                'bomb': save_data['bomb'],
+                'laser': save_data['laser'],
+                'red_dress': save_data['red_dress'],
+                'blu_dress': save_data['blu_dress'],
+                'green_dress': save_data['green_dress'],
+                'yellow_dress': save_data['yellow_dress'],
+                'selected_dress': save_data['selected_dress'],
+                'selected_projectiles': save_data['selected_projectiles']
+            }
+
+            # Aggiungi i nuovi dati al dizionario con timestamp come chiave
+            all_data[timestamp] = updated_data
+            
+            # Salva il dizionario aggiornato nel file JSON
+            with open(filename, 'w') as f:
+                json.dump(all_data, f, indent=4)
     
     def load_screen(self, timestamp):
         grid = self.ids.ammo_grid
@@ -120,7 +236,7 @@ class Projectile(Screen):
             if save_data['bullet'] == True:
                 button = ToggleButton(text=f"bullet",
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='bullet': self.select_projectile(dr)
+                                on_press=lambda btn, dr='bullet', ts=timestamp: self.select_projectile(dr, ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -132,7 +248,7 @@ class Projectile(Screen):
             if save_data['bomb'] == True:
                 button = ToggleButton(text=f"bomb",
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='bomb': self.select_projectile(dr)
+                                on_press=lambda btn, dr='bomb', ts=timestamp: self.select_projectile(dr, ts)
                                 )
                 grid.add_widget(button)
             else: 
@@ -144,7 +260,7 @@ class Projectile(Screen):
             if save_data['laser'] == True:
                 button = ToggleButton(text=f"laser",
                                 background_normal='images\captain.png',
-                                on_press=lambda btn, dr='laser': self.select_projectile(dr)
+                                on_press=lambda btn, dr='laser', ts=timestamp: self.select_projectile(dr, ts)
                                 )
                 grid.add_widget(button)
             else: 
